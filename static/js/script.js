486c1e4dd09d9ee800d225f43c8296e352eb9c04
casperdennijs <casper.dennijs@outlook.com>
2022-03-28 21:49:47 +0200

    Pre-Release version 2

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,17 @@ alert.addEventListener("click", () => {
     alert.remove();
 })
 
+const apple = document.querySelector("article.apple");
+const pokemon = document.querySelector("article.pokemon");
+const lyrics = document.querySelector("article.lyrics");
+
+apple.addEventListener("click", () => {
+    window.location.href = "portfolio/apple-remake.html";
+})
+pokemon.addEventListener("click", () => {
+    window.location.href = "portfolio/pokemon-battle.html";
+})
+lyrics.addEventListener("click", () => {
+    window.location.href = "portfolio/lyrics-video.html";
+})
+
